5643ff925356a2c2063492885aa9f8d915afdf2b
Sourcery AI <>
2023-08-29 08:32:03 +0000

    'Refactored by Sourcery'

--- healthcare_addon/healthcare_addon/page/rooms_monitor/rooms_monitor.py
+++ healthcare_addon/healthcare_addon/page/rooms_monitor/rooms_monitor.py
@@ -10,7 +10,8 @@ def get_patient_floor_drug(date):
     :param date: The date for which you want to get the data
     :return: A list of dicts.
     """
-    result = frappe.db.sql(f"""
+    return frappe.db.sql(
+        f"""
     select * from (select dosage_form,date,time,is_completed,drug_name,`tabInpatient Record`.name,gender,`tabInpatient Record`.patient  from `tabInpatient Medication Order` 
     join `tabInpatient Medication Order Entry` on `tabInpatient Medication Order`.name = `tabInpatient Medication Order Entry`.parent
     join `tabInpatient Record` on  `tabInpatient Record`.name = `tabInpatient Medication Order`.inpatient_record
@@ -25,8 +26,9 @@ def get_patient_floor_drug(date):
     ) as result2
     on result2.p_name = result1.patient
     ORDER BY TIME(time) asc
-    """,as_dict=True)
-    return result
+    """,
+        as_dict=True,
+    )
 
 
 @frappe.whitelist()
@@ -37,13 +39,15 @@ def get_patient_floor_section_room():
     record
     :return: A list of dictionaries.
     """
-    result = frappe.db.sql(f"""
+    return frappe.db.sql(
+        """
     select service_unit_type,patient_name,healthcare_service_unit_name,parent_healthcare_service_unit,`tabInpatient Record`.* from `tabInpatient Occupancy`
     join `tabHealthcare Service Unit` on `tabInpatient Occupancy`.service_unit = `tabHealthcare Service Unit`.name 
     join `tabInpatient Record` on `tabInpatient Record`.name = `tabInpatient Occupancy`.parent
     where `tabInpatient Occupancy`.left = 0
-    """,as_dict=True)
-    return result
+    """,
+        as_dict=True,
+    )
 
 @frappe.whitelist()
 def get_floor_section_room():
@@ -51,11 +55,11 @@ def get_floor_section_room():
     It returns a list of dictionaries, each dictionary representing a row in the table
     :return: A list of dictionaries.
     """
-    # select * from `tabHealthcare Service Unit`
-    # where is_group = 0
-    result = frappe.db.sql(f"""
+    return frappe.db.sql(
+        """
     select `tabHealthcare Service Unit`.* from `tabHealthcare Service Unit`
     left join `tabHealthcare Service Unit Type` on `tabHealthcare Service Unit Type`.name = `tabHealthcare Service Unit`.service_unit_type
     where `tabHealthcare Service Unit`.is_group = 0 and `tabHealthcare Service Unit Type`.allow_appointments = 0
-    """,as_dict=True)
-    return result
+    """,
+        as_dict=True,
+    )

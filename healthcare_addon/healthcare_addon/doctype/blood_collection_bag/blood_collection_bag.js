0dbc0d2adfeeebf002ef2adcfe42bba6f9547834
Abdullah <asalih@creativeadvtech.com>
2024-07-13 15:22:28 +0300

    update july 13

---
+++ healthcare_addon/healthcare_addon/doctype/blood_collection_bag/blood_collection_bag.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ahmed Ghazi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Blood Collection Bag", {
+// 	refresh(frm) {
+
+// 	},
+// });

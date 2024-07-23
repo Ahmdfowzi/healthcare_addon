9af9ecd2e2b1169860ee5cf87b860f0c548050c8
Osama Muhammed <42488643+osama1998H@users.noreply.github.com>
2024-07-23 09:22:13 +0000

    adjustment

--- healthcare_addon/healthcare_addon/doctype/blood_request/blood_request.py
+++ healthcare_addon/healthcare_addon/doctype/blood_request/blood_request.py
@@ -7,7 +7,7 @@ from frappe.model.document import Document
 
 class BloodRequest(Document):
 	def on_submit(self):
-		for bld in self.blood_request_execution:
+		for bld in self.collection_bag:
 			bag = frappe.get_doc("Blood Collection Bag",bld.blood_collection_bag)
 			bag.consumed = 1
 			bag.save()
--- healthcare_addon/healthcare_addon/doctype/blood_request_table/blood_request_table.py
+++ healthcare_addon/healthcare_addon/doctype/blood_request_table/blood_request_table.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class BloodRequestExecution(Document):
+class BloodRequestTable(Document):
 	pass

c5d86a584c1d00048787f57f0c77fa7022d69b29
Osama Muhammed <42488643+osama1998H@users.noreply.github.com>
2024-08-02 20:19:46 +0000

    fix return in blood request

--- healthcare_addon/healthcare_addon/doctype/blood_request/blood_request.py
+++ healthcare_addon/healthcare_addon/doctype/blood_request/blood_request.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class BloodRequest(Document):
@@ -11,3 +12,18 @@ class BloodRequest(Document):
 			bag = frappe.get_doc("Blood Collection Bag",bld.blood_collection_bag)
 			bag.consumed = 1
 			bag.save()
+
+
+
+@frappe.whitelist()
+def return_collection_bags(bag_names):
+	if isinstance(bag_names, str):
+		bag_names = frappe.parse_json(bag_names)
+
+	for bag_name in bag_names:
+		doc = frappe.get_doc("Blood Collection Bag", bag_name)
+		doc.consumed = 0
+		doc.returned = 1
+		doc.save()
+
+	return _("All selected bags have been processed.")
